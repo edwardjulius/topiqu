8268e919e4ef687a335d9acced0c5711f3c25da8
edwardjulius <edwardjulius@gmail.com>
2016-08-29 02:25:07 +0700

    add everything

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
   <div class="dropdown pull-left dropdown-tab">
     <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Today<b class="caret"></b></a>
     <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-      <li><a tabindex="-1" href="#">Action</a></li>
+      <li><a tabindex="1" href="#">Action</a></li>
       <li><a tabindex="-1" href="#">Another action</a></li>
       <li><a tabindex="-1" href="#">Something else here</a></li>
       <li class="divider"></li>
