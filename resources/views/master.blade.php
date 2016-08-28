088060a8294cca10c41dc784c96aea0c3a05898c
edwardjulius <edwardjulius@gmail.com>
2016-08-28 23:30:09 +0700

    add everything

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -28,12 +28,12 @@
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header navbar-btn">
+            <div class="navbar-header navbar-btn pull-left">
                 <button type="button" class="btn btn-default" aria-label="Menu">
                     <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
                 </button>
             </div>
-            <div class="navbar-header navbar-logo hidden-xs">
+            <div class="navbar-header pull-left hidden-xs">
                 <a class="navbar-brand" href="/">topiqu</a>
             </div>
             <div class="navbar-header pull-right">
