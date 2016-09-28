48767d520a08508677943cdf21d2d4379a5f8fa4
edwardjulius <edwardjulius@gmail.com>
2016-09-28 18:46:54 +0700

    asdf

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+@section('content')
+<div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">tentang topiqu.com</div>
+  <div class="panel-body">
+    <p></p>
+    <br>
+    <!-- 16:9 aspect ratio -->
+	<div class="embed-responsive embed-responsive-16by9">
+    	<iframe width="560" height="315" src="https://www.youtube.com/embed/-M9N9yqViWw" frameborder="0" allowfullscreen></iframe>
+	</div>
+ </div>
+@endsection
