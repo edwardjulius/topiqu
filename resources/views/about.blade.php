76765d689119c44f6d08f84223da9a7ac4a88102
edwardjulius <edwardjulius@gmail.com>
2016-10-01 04:28:49 +0700

    all

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,9 +2,11 @@
 @section('content')
 <div class="panel panel-default">
   <!-- Default panel contents -->
-  <div class="panel-heading">tentang kami</div>
+  <div class="panel-heading"><b>Tentang Kami</b></div>
   <div class="panel-body">
-  <p>Topiqu.com adalah komunitas</p>
+  	<p>topiqu.com adalah komunitas internet yang sangat percaya bahwa informasi adalah sesuatu aspek penting di dunia kami. Tidak hanya harus informasi itu dengan mudah bisa di bagi, tetapi juga harus bisa membuat process itu menyenangkan dan menarik. Di topiqu.com kami menyambut masyarakat Indonesia untuk ruang internet di mana orang dapat terinspirasi, terhibur, dan selalu diinformasikan oleh berita terbaru.
+  	<br><br>
+	Pelajari lebih lanjut tentang topiqu.com dengan menonton video di bawah:</p>
 	<div class="embed-responsive embed-responsive-16by9">
     	<iframe width="560" height="315" src="https://www.youtube.com/embed/-M9N9yqViWw" frameborder="0" allowfullscreen></iframe>
 	</div>
