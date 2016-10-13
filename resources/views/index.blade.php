6ed6e594d49a3a0227a60bda2f2a29168c6c9c67
edwardjulius <edwardjulius@gmail.com>
2016-10-14 04:44:00 +0700

    all

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,51 +9,10 @@
     <a href="#new-panel" class="mdl-tabs__tab">New</a>
   </div>
   <div class="mdl-tabs__panel is-active" id="trending-panel">
-  <center>
-<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-1">
-  <input type="radio" id="option-1" class="mdl-radio__button" name="options" value="1" checked>
-  <span class="mdl-radio__label">24 jam</span>
-</label>
-<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-2">
-  <input type="radio" id="option-2" class="mdl-radio__button" name="options" value="2">
-  <span class="mdl-radio__label">1 minggu</span>
-</label>
-<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-3">
-  <input type="radio" id="option-3" class="mdl-radio__button" name="options" value="3">
-  <span class="mdl-radio__label">1 bulan</span>
-</label>
-<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-4">
-  <input type="radio" id="option-4" class="mdl-radio__button" name="options" value="4">
-  <span class="mdl-radio__label">1 tahun</span>
-</label>
-<label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="option-5">
-  <input type="radio" id="option-5" class="mdl-radio__button" name="options" value="5">
-  <span class="mdl-radio__label">semua</span>
-</label>
-</center>
-    <ul>
-      <li>Eddard</li>
-      <li>Catelyn</li>
-      <li>Robb</li>
-      <li>Sansa</li>
-      <li>Brandon</li>
-      <li>Arya</li>
-      <li>Rickon</li>
-    </ul>
   </div>
   <div class="mdl-tabs__panel" id="top-panel">
-    <ul>
-      <li>Tywin</li>
-      <li>Cersei</li>
-      <li>Jamie</li>
-      <li>Tyrion</li>
-    </ul>
   </div>
   <div class="mdl-tabs__panel" id="new-panel">
-    <ul>
-      <li>Viserys</li>
-      <li>Daenerys</li>
-    </ul>
   </div>
 </div>
 @endsection
--- resources/views/master2.blade.php
+++ resources/views/master2.blade.php
@@ -37,53 +37,53 @@
             .mdl-layout__header-row {padding-left: 24px !important;}
         </style>
 
-        <body>
-            <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-                <header class="mdl-layout__header">
-                    <div class="mdl-layout__header-row">
-                        <!-- Title -->
-                        <span class="mdl-layout-title">
-                            <a href="/"><img alt="Brand" src="{{ asset('/topiqulogo.png') }}" width="35px" height="35px"></a>
-                        </span>
-                        <!-- Add spacer, to align navigation to the right -->
-                        <div class="mdl-layout-spacer"></div>
-                            <!-- Navigation -->
-                            <nav class="mdl-navigation">
-                                <a class="mdl-navigation__link" href="/login">Masuk</a>
-                                <a class="mdl-navigation__link" href="/register">Daftar</a>
-                                <!-- Right aligned menu below button -->
-                                <button id="demo-menu-lower-right" class="mdl-button mdl-js-button mdl-button--icon">
-                                    <i class="material-icons">more_vert</i>
-                                </button>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <!-- Title -->
+                    <span class="mdl-layout-title">
+                        <a href="/"><img alt="Brand" src="{{ asset('/topiqulogo.png') }}" width="35px" height="35px"></a>
+                    </span>
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                        <!-- Navigation -->
+                        <nav class="mdl-navigation">
+                            <a class="mdl-navigation__link" href="/login">Masuk</a>
+                            <a class="mdl-navigation__link" href="/register">Daftar</a>
+                            <!-- Right aligned menu below button -->
+                            <button id="demo-menu-lower-right" class="mdl-button mdl-js-button mdl-button--icon">
+                                <i class="material-icons">more_vert</i>
+                            </button>
 
-                                <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">
-                                    <li class="mdl-menu__item">Some Action</li>
-                                    <li class="mdl-menu__item">Another Action</li>
-                                    <li disabled class="mdl-menu__item">Disabled Action</li>
-                                    <li class="mdl-menu__item">Yet Another Action</li>
-                                </ul>
-                            </nav>
+                            <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">
+                                <li class="mdl-menu__item">Post</li>
+                                <li class="mdl-menu__item">Browse</li>
+                                <li class="mdl-menu__item">Messages</li>
+                                <li class="mdl-menu__item">Profile</li>
+                                <li class="mdl-menu__item">Settings</li>
+                                <li class="mdl-menu__item">Logout</li>
+                            </ul>
+                        </nav>
                     </div>
-                </header>
-                <main class="mdl-layout__content">
-                    <div class="page-content">
+            </header>
+            <main class="mdl-layout__content">
+                <div class="page-content">
+                    <body>
                         @yield('content')
-                        <hr>
-                        <center>
-                            <footer class="mdl-mini-footer">
-                                <div class="mdl-mini-footer__center-section">
-                                    <div class="mdl-logo">© 2016 topiqu prima multimedia</div>
-                                    <ul class="mdl-mini-footer__link-list">
-                                        <li><a href="/about">tentang topiqu</a></li>
-                                        <li><a href="/contact">hubungi kami</a></li>
-                                        <li><a href="/karir">karir</a></li>
-                                        <li><a href="/wiki">wiki</a></li>
-                                    </ul>
-                                </div>
-                            </footer>
-                    </div>
-                </main>
-            </div>
-        </body>
+                    </body>
+                    <footer class="mdl-mini-footer">
+                        <div class="mdl-mini-footer__center-section">
+                            <div class="mdl-logo">© 2016 topiqu prima multimedia</div>
+                            <ul class="mdl-mini-footer__link-list">
+                                <li><a href="/about">tentang topiqu</a></li>
+                                <li><a href="/contact">hubungi kami</a></li>
+                                <li><a href="/karir">karir</a></li>
+                                <li><a href="/wiki">wiki</a></li>
+                            </ul>
+                        </div>
+                    </footer>
+                </div>
+            </main>
+        </div>
 </html>
 
