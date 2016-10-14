1cc3d0c3d66307816b3694397f9e114e510e0520
edwardjulius <edwardjulius@gmail.com>
2016-10-14 18:17:01 +0700

    all

--- resources/views/master2.blade.php
+++ resources/views/master2.blade.php
@@ -34,7 +34,7 @@
             margin-left: auto;
             margin-right: auto;
         }
-        .search-form {padding-left: 15px; padding-top: 3px;}
+        .search-form {padding-left: 15px; padding-top: 4px;}
         .mdl-layout__header-row {padding-left: 24px !important;}
     </style>
 
