2250e3ea9d0c9a630a837512b1a4e43f544988cb
edwardjulius <edwardjulius@gmail.com>
2016-10-14 17:31:05 +0700

    all

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,25 +21,23 @@
     <a href="#top-panel" class="mdl-tabs__tab">Top</a>
     <a href="#new-panel" class="mdl-tabs__tab">New</a>
   </div>
-  <br>
-  <center>
-  <!-- Left aligned time below button -->
-  <button id="demo-menu-lower-left" class="mdl-button mdl-js-button mdl-button--icon">
-    <i class="material-icons">watch_later</i>
-  </button>
-
-  <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
-    <li disabled class="mdl-menu__item">24 jam</li>
-    <li class="mdl-menu__item">1 minggu</li>
-    <li class="mdl-menu__item">1 bulan</li>
-    <li class="mdl-menu__item">1 tahun</li>
-    <li class="mdl-menu__item">semua</li>
-  </ul>
-  <!-- End of time button -->
-  </center>
-  <br>
   <div class="mdl-tabs__panel is-active" id="trending-panel">
     <center>
+      <br>
+      <!-- Left aligned time below button -->
+      <button id="demo-menu-lower-left" class="mdl-button mdl-js-button mdl-button--icon">
+        <i class="material-icons">watch_later</i>
+      </button>
+      <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-left">
+        <li disabled class="mdl-menu__item">24 jam</li>
+        <li class="mdl-menu__item">1 minggu</li>
+        <li class="mdl-menu__item">1 bulan</li>
+        <li class="mdl-menu__item">1 tahun</li>
+        <li class="mdl-menu__item">semua</li>
+      </ul>
+      <!-- End of time button -->
+      <br>
+      <br>
       @foreach ($queries as $query)
       <div class="demo-card-wide mdl-card mdl-shadow--2dp">
         <div class="mdl-card__title">
