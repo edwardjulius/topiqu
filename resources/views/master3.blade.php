33280bdd4c3459f2250e66c15732e9214439233e
edwardjulius <edwardjulius@gmail.com>
2016-10-16 17:40:41 +0700

    all

--- resources/views/master3.blade.php
+++ resources/views/master3.blade.php
@@ -20,7 +20,7 @@
   </style>
   	<nav>
   		<div class="nav-wrapper blue darken-2">
-      <form class="brand-logo">
+      <form>
         <div class="input-field">
           <input id="search" type="search" class="right blue darken-2 white-text" required>
           <label for="search"><i class="material-icons">search</i></label>
