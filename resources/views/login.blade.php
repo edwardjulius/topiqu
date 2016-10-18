48e396dcf9276ecfafa07d3688219643d751a4f7
edwardjulius <edwardjulius@gmail.com>
2016-10-19 02:48:20 +0700

    sticky footer

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,45 +1,42 @@
 @extends('master3')
 @section('content')
-<style>
-	.form-login {max-width: 400px; margin: auto; padding-top:30px;}
-	.input-group {padding: 2px;}
-	label{font-weight: normal;}
-</style>
-@if(count($errors))
-    @foreach($errors->all() as $error)
-        <div class="alert alert-danger" role="alert">
-			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-			<span class="sr-only">Error:</span>
-				{{ $error }}
-		</div>
-		<br>
-    @endforeach
-@endif
-<form class="form-login" method="POST" role="form" action="{{ url('/login') }}">
-{{ csrf_field() }}
-	<div class="panel panel-default">
-	  <div class="panel-body">
-			<div class="input-group">
-			  <span class="input-group-addon" id="username"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
-			  <input type="text" id="username" class="form-control" placeholder="Username" name="username" value="{{ old('username') }}" required>
-			</div>
-			<div class="input-group">
-				  <span class="input-group-addon" id="password"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span></span>
-			      <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+<div class="container">
+	@if(count($errors))
+	    @foreach($errors->all() as $error)
+	        <div class="alert alert-danger" role="alert">
+				<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+				<span class="sr-only">Error:</span>
+					{{ $error }}
 			</div>
 			<br>
-			<center>
-			<label class="unselectable"><input type="checkbox" value="rememberme"> Ingat akun</label>
-			<br>
-			<font size=2>
-			<a href="/forgot">Lupa kredensial?</a>
-			<br>
-			<a href="/register">Belum Daftar?</a>
-			</font>
-			</center>
-			<br>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>
-		</div>
+	    @endforeach
+	@endif
+	<div class="row">
+	    <form class="col s8 offset-s2" method="POST" role="form" action="{{ url('/login') }}">
+	    	{{ csrf_field() }}
+	      	<div class="row">
+	        	<div class="input-field col s12">
+	          		<input id="username" type="text" class="validate" name="username" value="{{ old('username') }}" required>
+	          		<label for="username">Username</label>
+	        	</div>
+	        	<div class="input-field col s12">
+	          		<input id="password" type="password" class="validate" name="password" required>
+	          		<label for="password">Password</label>
+	        	</div>
+	        	<p>
+	        		<center>
+				      	<input type="checkbox" id="rememberme" value="rememberme"/>
+				      	<label for="rememberme">Ingat Akun</label><br><br>
+		   				<a href="/forgot">Lupa kredensial?</a><br>
+		   				<a href="/register">Belum Daftar?</a>
+		   			</center>
+	   			</p>
+	   			<br>
+	   			<center>
+	      			<button class="btn btn-lg btn-primary btn-block pink accent-3" type="submit">Masuk</button>
+	      		</center>
+	      	</div>
+	    </form>
 	</div>
-</form>
+</div>
 @endsection
