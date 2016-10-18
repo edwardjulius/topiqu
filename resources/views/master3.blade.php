25f4103100b4002f5d2d623574773e932fada072
edwardjulius <edwardjulius@gmail.com>
2016-10-18 20:42:39 +0700

    svg favicon

--- resources/views/master3.blade.php
+++ resources/views/master3.blade.php
@@ -15,8 +15,11 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <!-- Master Title and Favicon -->
-    <title>topiqu &middot; topik pilihanqu</title>
-    <link rel="shortcut icon" href="{{ asset('/icon.svg') }}">
+    <title>topiqudotcom</title>
+    <link rel="icon" href="{{ asset('/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+
   </head>
   <body class="indigo lighten-5">
     <style>
